dbaae927efa91be74b38ec9465d6a7cc566197d3
Melvin <101116598+Melboss28@users.noreply.github.com>
2024-06-18 16:55:19 +0200

    AuthMiddleware JWT

---
+++ API/JWT/authMiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+function authMiddleware(req, res, next) {
+    const authHeader = req.header('Authorization');
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Accès refusé. Aucun jeton fourni.' });
+    }
+
+    const token = authHeader.substring(7); // Supprime le préfixe 'Bearer '
+
+    try {
+        const decoded = jwt.verify(token, process.env.KEY);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(400).json({ message: 'Session expiré. Reconnexion demandé.' });
+    }
+}
+
+module.exports = authMiddleware;
