be6eac007cfde767a4d83655019093a0e24eeec3
Melboss28 <maubril28@gmail.com>
2024-06-23 22:43:15 +0200

    login with navbar

--- WEB/src/pages/auth/Login.js
+++ WEB/src/pages/auth/Login.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LoginForm from '../../components/LoginForm';
 import VerticalNavbar from '../../components/VerticalNavbar';
+import './Login.css';
 
 const Login = () => {
-    return (
-        <>
-            <VerticalNavbar/>
-            <LoginForm/>
-        </>
-    );
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div className="login-page">
+      <VerticalNavbar isOpen={isOpen} setIsOpen={setIsOpen} />
+      <div className={`main-content ${isOpen ? 'open' : 'closed'}`}>
+        <LoginForm />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
