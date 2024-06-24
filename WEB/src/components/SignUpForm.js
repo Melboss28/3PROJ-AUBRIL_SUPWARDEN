0bc506bc6ea2baa2ecd811c605a8145062e73080
Melboss28 <maubril28@gmail.com>
2024-06-24 23:58:40 +0200

    login/singup with api

--- WEB/src/components/SignUpForm.js
+++ WEB/src/components/SignUpForm.js
@@ -1,32 +1,68 @@
-// src/components/SignUpForm.js
-import React from 'react';
-import './LoginForm.css'; // Réutiliser les mêmes styles
+import React, {useState} from 'react';
+// import { Link } from 'react-router-dom';
+// import { GoogleLogin } from '@react-oauth/google';
+import './LoginForm.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { accountService } from '../_services/account.service';
 
 const SignUpForm = () => {
+  const [pseudo, setPseudo] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  // const [errorGoogleMessage, setErrorGoogleMessage] = useState('');
+  const [goodMessage, setGoodMessage] = useState('');
+  // const [goodGoogleMessage, setGoodGoogleMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+        const response = await axios.post(`http://localhost:3001/api/user/register`, { email, pseudo, password });
+        // Réinitialiser le message d'erreur en cas de succès
+        setErrorMessage('');
+        setGoodMessage(response.data.message);
+    } catch (error) {
+        setGoodMessage('');
+        if (error.response && error.response.data && error.response.data.message) {
+            // Si la réponse de l'API contient un message d'erreur
+            const errorMessage = error.response.data.message;
+            console.error('Erreur de réponse de l\'API:', errorMessage);
+            setErrorMessage(errorMessage);
+        } else {
+            // Si la réponse de l'API ne contient pas de message d'erreur, afficher un message générique
+            console.error('Erreur:', error.message);
+            setErrorMessage('Une erreur est survenue.');
+        }
+    }
+  };
+
   return (
     <div className="login-container">
       <div className="login-box">
         <h2>Inscription</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-group">
             <label htmlFor="username">Nom d'utilisateur</label>
-            <input type="text" id="username" name="username" required />
+            <input type="text" value={pseudo} onChange={(e) => setPseudo(e.target.value)} required />
           </div>
           <div className="input-group">
             <label htmlFor="email">Adresse Email</label>
-            <input type="email" id="email" name="email" required />
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
           </div>
           <div className="input-group">
             <label htmlFor="password">Mot de passe</label>
-            <input type="password" id="password" name="password" required />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
           </div>
           <div className="input-group">
             <label htmlFor="confirm-password">Confirmer le mot de passe</label>
-            <input type="password" id="confirm-password" name="confirm-password" required />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
           </div>
           <div className="login-button-container">
             <button type="submit" className="login-button">S'inscrire</button>
           </div>
+          {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
+          {goodMessage && <div style={{ color: 'green' }}>{goodMessage}</div>}
         </form>
       </div>
     </div>
--- WEB/src/pages/auth/SignUp.js
+++ WEB/src/pages/auth/SignUp.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SignUpForm from '../../components/SignUpForm';
+import VerticalNavbar from '../../components/VerticalNavbar';
+import './Login.css';
 
 const SignUp = () => {
+    const [isOpen, setIsOpen] = useState(true);
+    
     return (
-        <SignUpForm/>
+        <div className="login-page">
+            <VerticalNavbar isOpen={isOpen} setIsOpen={setIsOpen} />
+            <div className={`main-content ${isOpen ? 'open' : 'closed'}`}>
+            <SignUpForm />
+            </div>
+      </div>
     );
 };
 
