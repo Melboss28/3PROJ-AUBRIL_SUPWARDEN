87ef89d79a1be2ee420a6f80c7a52ba5372835b9
Melboss28 <maubril28@gmail.com>
2024-06-28 23:57:55 +0200

    Home init

--- WEB/src/pages/home/HomeRouter.js
+++ WEB/src/pages/home/HomeRouter.js
@@ -1,14 +1,14 @@
 import React from 'react';
-// import Home from './Home';
+import Home from './Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Error from '../../_utils/Error';
 
 const HomeRouter = () => {
     return (
         <Routes>
-          {/* <Route index element={<Home/>}/> */}
+          <Route index element={<Home/>}/>
           
-          {/* <Route path="/home" element={<Home/>}/> */}
+          <Route path="/home" element={<Home/>}/>
 
           <Route path='*' element={<Error/>}/>
         </Routes>
